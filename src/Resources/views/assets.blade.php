b7e794a49c4b6aef4af0d791a3b3dd468a0968b0
Shadow <admin@hongyuvip.com>
2019-07-26 23:15:39 +0800

    Initial commit

--- src/Resources/views/assets.blade.php
+++ src/Resources/views/assets.blade.php
@@ -1,5 +1,7 @@
+<!-- 配置文件 -->
 <script type="text/javascript" src="{{ asset('vendor/ueditor/ueditor.config.js') }}"></script>
-<script type="text/javascript" src="{{ asset('vendor/ueditor/ueditor.all.js') }}"></script>
+<!-- 编辑器源码文件 -->
+<script type="text/javascript" src="{{ asset('vendor/ueditor/ueditor.all.min.js') }}"></script>
 @if($locale = strtolower(config('app.locale','zh-CN')))
 <script type="text/javascript" src="{{ asset("vendor/ueditor/lang/{$locale}/{$locale}.js") }}"></script>
 @endif
