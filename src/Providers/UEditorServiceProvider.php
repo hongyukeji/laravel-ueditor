9857cdce37b190d6e7eab54c698ca8c607f9203e
Shadow <admin@hongyuvip.com>
2019-07-26 22:01:10 +0800

    init

--- src/Providers/UEditorServiceProvider.php
+++ src/Providers/UEditorServiceProvider.php
@@ -10,18 +10,18 @@ class UEditorServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/Resources/views', 'UEditor');
+        $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'UEditor');
 
         $this->publishes([
-            __DIR__ . '/Config/ueditor.php' => config_path('ueditor.php'),
+            __DIR__ . '/../Config/ueditor.php' => config_path('ueditor.php'),
         ], 'config');
 
         $this->publishes([
-            realpath(__DIR__ . '/Resources/views') => base_path('Resources/views/vendor/ueditor'),
+            realpath(__DIR__ . '/../Resources/views') => base_path('Resources/views/vendor/ueditor'),
         ], 'view');
 
         $this->publishes([
-            realpath(__DIR__ . '/Resources/assets/ueditor') => public_path('vendor/ueditor'),
+            realpath(__DIR__ . '/../Resources/assets/ueditor') => public_path('vendor/ueditor'),
         ], 'assets');
 
         $this->registerRoute();
@@ -29,7 +29,7 @@ class UEditorServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/Config/ueditor.php', 'ueditor');
+        $this->mergeConfigFrom(__DIR__ . '/../Config/ueditor.php', 'ueditor');
         $this->app->singleton('ueditor.storage', function ($app) {
             return new StorageManager(Storage::disk($app['config']->get('ueditor.disk', 'public')));
         });
